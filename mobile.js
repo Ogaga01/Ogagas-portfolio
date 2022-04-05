0924c122df7a4274b2bfc16d7c4fa8236fe6ea87
Ogaga <iyaraogaga69@gmail.com>
2022-04-05 11:10:07 +0100

    Fix linters errors

--- mobile.js
+++ mobile.js
@@ -1,20 +1,20 @@
-const openMenu = document.getElementById('menu-up')
-const closeMenu = document.querySelector(".close")
-const popUp = document.querySelector(".menu-list")
-const listItem =document.querySelectorAll('.list')
+const openMenu = document.getElementById('menu-up');
+const closeMenu = document.querySelector('.close');
+const popUp = document.querySelector('.menu-list');
+const listItem = document.querySelectorAll('.list');
 
 openMenu.addEventListener('click', () => {
-popUp.classList.add('show')
-})
+  popUp.classList.add('show');
+});
 openMenu.addEventListener('click', () => {
-    listItem.style.display = "block"
-})
-closeMenu.addEventListener("click", () => {
-    popUp.classList.remove('show')
-})
+  listItem.style.display = 'block';
+});
+closeMenu.addEventListener('click', () => {
+  popUp.classList.remove('show');
+});
 
 listItem.forEach((item) => {
-    item.addEventListener('click', () => {
-        popUp.classList.remove('show')
-    })
-})
+  item.addEventListener('click', () => {
+    popUp.classList.remove('show');
+  });
+});
